ae8666bc2df5f3981cd4ed2749f714f415bf9518
Michael Petersen <railfan@drgw.net>
2023-03-23 09:06:57 -0600

    Updated to Python3

--- doc/xbee/verify.py
+++ doc/xbee/verify.py
@@ -4,8 +4,8 @@ import argparse
 import sys
 
 def xbeeParam(param, expectedValue):
-	ser.write(param + ",")
-	read_val = ser.read(size=64).rstrip()
+	ser.write(bytes(param + ',', 'utf-8'))
+	read_val = ser.read(size=64).rstrip().decode()
 	sys.stdout.write(param + "=" + read_val)
 	if(expectedValue == int(read_val,16)):   # ID requires hex, use hex for all to make things simple
 		sys.stdout.write(u"\u001b[32m OK\u001b[0m\n")
@@ -23,23 +23,23 @@ try:
 	ser.open()
 	sys.stdout.write("Entering command mode... ")
 	time.sleep(1)
-	ser.write("+++")
+	ser.write(b'+++')
 	time.sleep(1)
 	read_val = ser.read(size=64).rstrip()
-	sys.stdout.write(read_val + "" + "\n")
+	sys.stdout.write(read_val.decode() + "" + "\n")
 
-	print "Reading..."
-	ser.write("AT")
+	print("Reading...")
+	ser.write(b'AT')
 
 	xbeeParam("ID", 0x225)
 	xbeeParam("RR", 6)
 	xbeeParam("RN", 3)
 #	xbeeParam("PL", 2)
-        xbeeParam("PP", 8)
+	xbeeParam("PP", 8)
 	xbeeParam("SM", 0)
 	xbeeParam("BD", 7)
 	xbeeParam("AP", 2)
 
 except serial.SerialException:
-	print "Error opening serial port"
+	print("Error opening serial port")
 
